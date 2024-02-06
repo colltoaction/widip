0fdbd3b276bf14e7d73d29891f6c03687a0f4c7a
Martin Coll <mcoll@dc.uba.ar>
2024-02-06 12:12:02 -0300

    test bool maybe either

--- test_loader.py
+++ test_loader.py
@@ -1,17 +1,25 @@
 import yaml
-from discopy.frobenius import Box, Ty, Spider, Diagram
+from discopy.frobenius import Box, Ty, Spider, Diagram, Id
 
 from loader import HypergraphLoader
 
-def test_monoid():
-    d = (
-            Spider(0, 1, Ty("0")) @ Box("μ", Ty("μ"), Ty("μ")) >> \
-            Box("0", Ty("0"), Ty("0")) @ Spider(1, 0, Ty("μ"))
-        ) @ \
-        (
-            Spider(0, 1, Ty("2")) @ Box("η", Ty("η"), Ty("η")) >> \
-            Box("2", Ty("2"), Ty("2")) @ Spider(1, 0, Ty("η"))
-        )
-    t = yaml.compose(open("src/yaml/monoid.yaml"), Loader=HypergraphLoader)
+def test_bool():
+    d = Box("true", Ty("true"), Ty("true")) @ \
+        Box("false", Ty("false"), Ty("false"))
+    t = yaml.compose(open("src/yaml/data/bool.yaml"), Loader=HypergraphLoader)
+    with Diagram.hypergraph_equality:
+        assert t.to_diagram() == d
+
+def test_maybe():
+    d = Id(Ty("nothing")) @ \
+        Box("a", Ty("just"), Ty("just"))
+    t = yaml.compose(open("src/yaml/data/maybe.yaml"), Loader=HypergraphLoader)
+    with Diagram.hypergraph_equality:
+        assert t.to_diagram() == d
+
+def test_either():
+    d = Box("a", Ty("left"), Ty("left")) @ \
+        Box("b", Ty("right"), Ty("right"))
+    t = yaml.compose(open("src/yaml/data/either.yaml"), Loader=HypergraphLoader)
     with Diagram.hypergraph_equality:
         assert t.to_diagram() == d
