c7aa4b761ab00fc807709fd6f0abe6e50ea79aae
Martin Coll <mcoll@dc.uba.ar>
2024-02-06 12:12:01 -0300

    frobenius spiders look reasonable

--- __main__.py
+++ __main__.py
@@ -20,36 +20,35 @@ def compose_graphs(graphs):
     for H in graphs:
         boundary >>= Id().tensor(*graph_spider_boundary(G, H))
         G = H
-    return boundary
+    return boundary or Id()
 
 def graph_spider_boundary(G, H):
     # importa el orden de cables no de cajas
     for node in sorted(set(H.nodes).union(G.nodes)):
-        # sum(1 for p in G.predecessors(node) if p) if node in G else 1,
-        # sum(1 for s in H.successors(node) if s) if node in H else 1,
-        # if not node:
-        #     continue
         n_legs_in = 0
-        if node in G:
-            n_legs_in += G.in_degree(node)
-        if node in H:
-            n_legs_in += H.in_degree(node)
-        # if n_legs_in == 0:
-        #     n_legs_in += 1
-        # if G.has_edge('', node):
-        #     n_legs_in += 1
         n_legs_out = 0
-        if node in G:
-            n_legs_out += G.out_degree(node)
-        if node in H:
-            n_legs_out += H.out_degree(node)
-        # if n_legs_out == 0:
-        #     n_legs_out += 1
-        # if H.has_edge(node, ''):
-        #     n_legs_out += 1
-        if node:
-            spider = Spider(n_legs_in, n_legs_out, Ty(node))
-            yield spider
+        if not node:
+            continue
+        if node in G and node in H:
+            G_in = G.in_degree(node)
+            G_out = G.out_degree(node)
+            H_in = H.in_degree(node)
+            H_out = H.out_degree(node)
+            n_legs_in += G_in + H_in
+            n_legs_out += G_out + H_out
+        elif node in G:
+            G_out = G.out_degree(node)
+            n_legs_in += G_out
+            if G_out == 0:
+                n_legs_in += 1
+        elif node in H:
+            H_in = H.in_degree(node)
+            n_legs_out += H_in
+            if H_in == 0:
+                n_legs_out += 1
+
+        spider = Spider(n_legs_in, n_legs_out, Ty(node))
+        yield spider
 
 def path_edges(path: pathlib.Path):
     for subpath in path.iterdir():
@@ -61,7 +60,11 @@ def compose_graph_file(path: pathlib.Path):
     if path.is_dir():
         G = [nx.DiGraph(path_edges(path))]
     else:
-        G = yaml.compose_all(open(path), Loader=NxSafeLoader)
+        root = nx.DiGraph()
+        root.add_node(path.stem)
+        G = itertools.chain(
+            [root],
+            yaml.compose_all(open(path), Loader=NxSafeLoader))
     G = compose_graphs(G)
     # TODO temporary path
     G.to_gif(path=path.with_suffix(".gif"), loop=True)
