d136ef06de818f84db8bb842cdbbe8fc2cfba11f
Martin Coll <mcoll@dc.uba.ar>
2024-02-10 12:54:00 -0300

    introduce eval box

--- files.py
+++ files.py
@@ -3,7 +3,9 @@ import pathlib
 import sys
 
 import yaml
-from discopy.frobenius import Ty, Diagram, Hypergraph as H, Box, Functor, Spider, Swap
+from discopy.frobenius import Ty, Diagram, Hypergraph as H, Box, Functor, Spider, Swap, Category, Id, Ob
+from discopy.closed import Eval
+from discopy import python
 
 from loader import HypergraphLoader, compose_entry
 
@@ -11,19 +13,40 @@ def swap(box):
     (l, r) = tuple(map(Ty, box.cod.inside))
     return Swap(r, l)
 
-boxes = {
-    'swap': swap
+def eval_functor(boxes):
+    ob = {}
+    ar = {}
+    for box in boxes:
+        # TODO replacing ob clashes with other uses
+        if box.name == "eval":
+            (source_dom, ) = box.dom.inside
+            (source_cod, ) = box.cod.inside
+            eval_result = str(eval(source_dom.name) or "")
+            ob[Ty(source_dom)] = Ty(eval_result)
+            ob[Ty(source_cod)] = Ty(eval_result)
+            # TODO replace wires
+            ar[box] = Id(Ty(eval_result))
+    return Functor(lambda x: ob.get(x, x), lambda x: ar.get(x, x))
+
+native_boxes = {
+    'swap': swap,
 }
 def compose_graphs(graphs):
     graph = functools.reduce(
+        # compose_entry,
         lambda k, v: k >> v,
         graphs)
     f = Functor(
         ob=lambda x: x,
-        ar={box: boxes[box.name](box) if box.name in boxes else box for box in graph.boxes})
+        ar={box: native_boxes[box.name](box) if box.name in native_boxes else box for box in graph.boxes})
     graph = f(graph.to_diagram())
+    f2 = eval_functor(graph.boxes)
+    # graph.draw()
+    graph = f2(graph)
     return graph
 
+# TODO expose this in the DSL to start bootstrapping.
+# e.g detect !tag when tag dir and/or tag.yaml are present.
 def compose_graph_file(path: pathlib.Path):
     if path.is_dir():
         # TODO
