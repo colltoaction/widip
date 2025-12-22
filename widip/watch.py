8757d2e1aa71050c5102118909cf110ea4c97cf4
Martin Coll <mcoll@dc.uba.ar>
2025-12-22 16:59:35 +0000

    migrate from watchdog to watchfiles

--- widip/watch.py
+++ widip/watch.py
@@ -1,8 +1,7 @@
 import asyncio
 import sys
 from pathlib import Path
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
+from watchfiles import awatch
 from yaml import YAMLError
 
 from discopy.utils import tuplify
@@ -15,42 +14,29 @@ from .compiler import SHELL_COMPILER, force
 
 # TODO watch functor ??
 
-class ShellHandler(FileSystemEventHandler):
-    """Reload the shell on change."""
-    def __init__(self, loop, queue):
-        self.loop = loop
-        self.queue = queue
+def reload_diagram(path_str):
+    print(f"reloading {path_str}")
+    try:
+        fd = file_diagram(path_str)
+        diagram_draw(Path(path_str), fd)
+        diagram_draw(Path(path_str+".2"), fd)
+    except YAMLError as e:
+        print(e)
 
-    def on_modified(self, event):
-        if event.src_path.endswith(".yaml"):
-            self.loop.call_soon_threadsafe(self.queue.put_nowait, event)
-
-async def handle_events(queue):
-    while True:
-        event = await queue.get()
-        print(f"reloading {event.src_path}")
-        try:
-            fd = file_diagram(str(event.src_path))
-            diagram_draw(Path(event.src_path), fd)
-            diagram_draw(Path(event.src_path+".2"), fd)
-        except YAMLError as e:
-            print(e)
-        queue.task_done()
+async def handle_changes():
+    # watchfiles awatch yields sets of changes
+    async for changes in awatch('.', recursive=True):
+        for change_type, path_str in changes:
+             if path_str.endswith(".yaml"):
+                 reload_diagram(path_str)
 
 async def async_shell_main(file_name):
     path = Path(file_name)
     loop = asyncio.get_running_loop()
-    queue = asyncio.Queue()
 
-    # Start observer
+    # Start watcher
     print(f"watching for changes in current path")
-    event_handler = ShellHandler(loop, queue)
-    observer = Observer()
-    observer.schedule(event_handler, ".", recursive=True)
-    observer.start()
-
-    # Start event consumer
-    consumer_task = asyncio.create_task(handle_events(queue))
+    watcher_task = asyncio.create_task(handle_changes())
 
     try:
         while True:
@@ -77,9 +63,11 @@ async def async_shell_main(file_name):
             except YAMLError as e:
                 print(e)
     finally:
-        observer.stop()
-        observer.join()
-        consumer_task.cancel()
+        watcher_task.cancel()
+        try:
+            await watcher_task
+        except asyncio.CancelledError:
+            pass
 
 async def async_widish_main(file_name, *shell_program_args: str):
     loop = asyncio.get_running_loop()
