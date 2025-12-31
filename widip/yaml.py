5629e63c346ba0b3fd8a07f4723fd6a9be5c1b8f
Martin Coll <mcoll@dc.uba.ar>
2025-12-31 15:57:39 +0000

    more contextlib

--- widip/yaml.py
+++ widip/yaml.py
@@ -49,55 +49,12 @@ class Sequence(closed.Box):
     def n(self):
         return len(self.dom)
 
-    @property
-    def exps(self):
-        return self.cod[0].inside[0].exponent if self.cod and self.cod[0].is_exp else closed.Ty()
-
-    @property
-    def bases(self):
-        return self.cod[0].inside[0].base if self.cod and self.cod[0].is_exp else closed.Ty()
-
-    @staticmethod
-    @contextmanager
-    def load(ob, tag=None):
-        if tag:
-            bases, exps = get_exps_bases(ob.cod)
-            ob = (bases @ ob >> closed.Eval(bases >> exps))
-            ob >>= Node(tag, ob.cod, closed.Ty() >> closed.Ty(tag))
-        token = diagram_var.set(ob)
-        try:
-            yield
-        finally:
-            diagram_var.reset(token)
-
 class Mapping(closed.Box):
     def __init__(self, dom, cod=None):
         if cod is None:
             exps, bases = get_exps_bases(dom)
             cod = bases << exps
         super().__init__("Mapping", dom, cod)
-
-    @property
-    def exps(self):
-        return self.cod[0].inside[0].exponent if self.cod and self.cod[0].is_exp else closed.Ty()
-
-    @property
-    def bases(self):
-        return self.cod[0].inside[0].base if self.cod and self.cod[0].is_exp else closed.Ty()
-
-    @staticmethod
-    @contextmanager
-    def load(diag, tag=None):
-        exps, bases = get_exps_bases(diag.cod)
-        ob = diag >> Mapping(diag.cod)
-        if tag:
-            ob = (ob @ exps >> closed.Eval(bases << exps))
-            ob >>= Node(tag, ob.cod, closed.Ty(tag) >> closed.Ty(tag))
-        token = diagram_var.set(ob)
-        try:
-            yield
-        finally:
-            diagram_var.reset(token)
 
 class Anchor(closed.Box):
     def __init__(self, name, dom, cod):
@@ -110,6 +67,29 @@ class Alias(closed.Box):
 Yaml = closed.Category()
 
 
+@contextmanager
+def load_container(ob, tag, is_sequence):
+    if not is_sequence:
+        ob >>= Mapping(ob.cod)
+    
+    if tag:
+        target = ob.cod
+        exps, bases = get_exps_bases(target)
+        if is_sequence:
+            ob = exps @ ob
+            ob >>= closed.Eval(target)
+            ob >>= Node(tag, ob.cod, closed.Ty() >> closed.Ty(tag))
+        else:
+            ob @= exps
+            ob >>= closed.Eval(target)
+            ob >>= Node(tag, ob.cod, closed.Ty(tag) >> closed.Ty(tag))
+            
+    token = diagram_var.set(ob)
+    try:
+        yield
+    finally:
+        diagram_var.reset(token)
+
 def get_exps_bases(cod):
     exps = closed.Ty().tensor(*[x.inside[0].exponent for x in cod])
     bases = closed.Ty().tensor(*[x.inside[0].base for x in cod])
@@ -119,34 +99,15 @@ def load_scalar(cursor, tag):
     data = hif.get_node_data(cursor)
     return Scalar(tag, data["value"])
 
-def load_sequence(cursor, tag=None):
-    items = list(map(_incidences_to_diagram, hif.iterate(cursor)))
+def load_collection(cursor, tag, is_sequence, reduce_fn, transform_items=lambda x: x):
+    diagrams = map(_incidences_to_diagram, hif.iterate(cursor))
+    items = list(transform_items(diagrams))
     if not items:
         if tag: return Node(tag, closed.Ty(), closed.Ty(tag) >> closed.Ty(tag))
         return closed.Id()
     
-    def reduce_fn(acc, val):
-        combined = acc @ val
-        return combined >> Sequence(combined.cod)
-        
     ob = functools.reduce(reduce_fn, items)
-    with Sequence.load(ob, tag):
-        return diagram_var.get()
-
-def load_mapping(cursor, tag=None):
-    diagrams = map(_incidences_to_diagram, hif.iterate(cursor))
-    items = list(batched(diagrams, 2))
-    def load_pair(pair):
-        key, value = pair
-        return key @ value >> Sequence(key.cod @ value.cod, n=2)
-    
-    kv_diagrams = [load_pair(p) for p in items]
-    if not kv_diagrams:
-        if tag: return Node(tag, closed.Ty(), closed.Ty(tag) >> closed.Ty(tag))
-        return closed.Id()
-        
-    ob = functools.reduce(lambda a, b: a @ b, kv_diagrams)
-    with Mapping.load(ob, tag):
+    with load_container(ob, tag, is_sequence):
         return diagram_var.get()
 
 def incidences_to_diagram(node: HyperGraph):
@@ -167,16 +128,25 @@ def _incidences_to_diagram(cursor):
         case "scalar":
             ob = load_scalar(cursor, tag)
         case "sequence":
-            ob = load_sequence(cursor, tag)
+            def reduce_seq(a, b):
+                a @= b
+                a >>= Sequence(a.cod)
+                return a
+            ob = load_collection(cursor, tag, True, reduce_seq)
         case "mapping":
-            ob = load_mapping(cursor, tag)
+            def transform_mapping_items(diagrams):
+                for key, val in batched(diagrams, 2):
+                    key @= val
+                    key >>= Sequence(key.cod, n=2)
+                    yield key
+            ob = load_collection(cursor, tag, False, lambda a, b: a @ b, transform_mapping_items)
         case "alias":
             ob = Alias(anchor, closed.Ty(), closed.Ty() >> closed.Ty(anchor))
         case _:
             raise Exception(f"Kind \"{kind}\" doesn't match any.")
 
     if anchor and kind != 'alias':
-        ob = ob >> Anchor(anchor, ob.cod, ob.cod)
+        ob >>= Anchor(anchor, ob.cod, ob.cod)
     return ob
 
 def load_document(cursor):
