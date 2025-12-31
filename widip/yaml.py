f3cc8dfa9beb07d3d7f56c4953713c83f7b04362
Martin Coll <mcoll@dc.uba.ar>
2025-12-31 15:44:22 +0000

    contextlib

--- widip/yaml.py
+++ widip/yaml.py
@@ -1,9 +1,13 @@
 import functools
+from contextlib import contextmanager
+from contextvars import ContextVar
 from itertools import batched
 from discopy import closed
 from nx_hif.hif import HyperGraph
 from . import hif
 
+
+diagram_var: ContextVar[closed.Diagram] = ContextVar("diagram")
 
 class Node(closed.Box):
     def __init__(self, name, dom, cod):
@@ -54,12 +58,17 @@ class Sequence(closed.Box):
         return self.cod[0].inside[0].base if self.cod and self.cod[0].is_exp else closed.Ty()
 
     @staticmethod
+    @contextmanager
     def load(ob, tag=None):
         if tag:
-            exps, bases = get_exps_bases(ob.cod)
-            ob = (exps @ ob >> closed.Eval(ob.cod))
+            bases, exps = get_exps_bases(ob.cod)
+            ob = (bases @ ob >> closed.Eval(bases >> exps))
             ob >>= Node(tag, ob.cod, closed.Ty() >> closed.Ty(tag))
-        return ob
+        token = diagram_var.set(ob)
+        try:
+            yield
+        finally:
+            diagram_var.reset(token)
 
 class Mapping(closed.Box):
     def __init__(self, dom, cod=None):
@@ -77,13 +86,18 @@ class Mapping(closed.Box):
         return self.cod[0].inside[0].base if self.cod and self.cod[0].is_exp else closed.Ty()
 
     @staticmethod
-    def load(ob, tag=None):
-        m = Mapping(ob.cod)
-        ob = ob >> m
+    @contextmanager
+    def load(diag, tag=None):
+        exps, bases = get_exps_bases(diag.cod)
+        ob = diag >> Mapping(diag.cod)
         if tag:
-            ob = (ob @ m.exps >> closed.Eval(m.cod))
+            ob = (ob @ exps >> closed.Eval(bases << exps))
             ob >>= Node(tag, ob.cod, closed.Ty(tag) >> closed.Ty(tag))
-        return ob
+        token = diagram_var.set(ob)
+        try:
+            yield
+        finally:
+            diagram_var.reset(token)
 
 class Anchor(closed.Box):
     def __init__(self, name, dom, cod):
@@ -116,7 +130,8 @@ def load_sequence(cursor, tag=None):
         return combined >> Sequence(combined.cod)
         
     ob = functools.reduce(reduce_fn, items)
-    return Sequence.load(ob, tag)
+    with Sequence.load(ob, tag):
+        return diagram_var.get()
 
 def load_mapping(cursor, tag=None):
     diagrams = map(_incidences_to_diagram, hif.iterate(cursor))
@@ -131,7 +146,8 @@ def load_mapping(cursor, tag=None):
         return closed.Id()
         
     ob = functools.reduce(lambda a, b: a @ b, kv_diagrams)
-    return Mapping.load(ob, tag)
+    with Mapping.load(ob, tag):
+        return diagram_var.get()
 
 def incidences_to_diagram(node: HyperGraph):
     cursor = (0, node)
