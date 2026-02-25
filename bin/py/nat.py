76f234a5548cc4b4a545091d5abe75001257b832
Agent Widip <agentwidip@gmail.com>
2026-02-25 19:46:33 +0000

    chore: delete old code

--- bin/py/nat.py
+++
@@ -1,9 +0,0 @@
-from src.data.nat import zero, succ, plus_box
-from .rep import py_functor
-
-
-py_nat_f = py_functor(
-    lambda ar: {
-        zero.name: lambda: 0,
-        succ.name: lambda x: int(x) + 1,
-        plus_box.name: lambda *xs: sum(xs),}[ar.name],)
