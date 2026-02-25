68150156c47e09521d59cc3b59f8425e8d9db26b
Agent Widip <agentwidip@gmail.com>
2026-02-25 19:46:33 +0000

    chore: delete old code

--- bin/py/bool.py
+++
@@ -1,8 +0,0 @@
-from .rep import py_functor
-
-
-py_bool_f = py_functor(
-    lambda ar: {
-        'true': lambda: True,
-        'false': lambda: False,
-        'and': lambda *xs: xs[0] and xs[1],}[ar.name],)
