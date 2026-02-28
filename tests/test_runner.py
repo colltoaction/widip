eb18b51806a9ac4433bef94c7903e19cf3d2d801
Agent Widip <agentwidip@gmail.com>
2026-02-28 16:35:56 +0000

    wip

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,7 +1,16 @@
 import pytest
 
+from discopy.utils import tuplify, untuplify
+
 from widip.loader import repl_read
 from widip.widish import SHELL_RUNNER
+
+
+# TODO constants have to be tested, not test code
+def run_diagram(fd, stdin):
+    constants = tuple(x.name for x in fd.dom)
+    result = SHELL_RUNNER(fd)(*constants)(stdin)
+    return result
 
 
 @pytest.mark.parametrize(["yaml_text", "stdin", "expected"], [
@@ -10,9 +19,16 @@ from widip.widish import SHELL_RUNNER
     ["- scalar",           "",   "scalar"],
     ["!printf scalar",     "",   "scalar"],
     ["!echo scalar",       "",   "scalar\n"],
+    ["{a, b, c}",          "",   ("a", "b", "c")],
+    ["{a, !echo b, c}",    "",   ("a", "b\n", "c")],
+    ["!printf { '%s %s %s', a, !echo b, c }", "", "a b\n c"],
+    ["!echo {a, b, c}",    "",   "a b c\n"],
+    ["!printf { '%s %s %s', a, b, c }", "", "a b c"],
+    # Test document-level tag
+    ["--- !echo doc content", "", "doc content"],
+    [open("examples/hello-world.yaml"), "", "Hello world!"],
+    [open("examples/shell.yaml"),       "", ('72', '22', '  ? !grep grep: !wc -c\n  ? !tail -2')],
 ])
 def test_shell_runner(yaml_text, stdin, expected):
-    # TODO deduplicate with widish_main
-    fd = repl_read(yaml_text)
-    constants = tuple(x.name for x in fd.dom)
-    assert SHELL_RUNNER(fd)(*constants)(stdin) == expected
+    result = run_diagram(repl_read(yaml_text), stdin)
+    assert result == expected
